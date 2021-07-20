3b1edcea49567cc06d9a83698c12dccd00f52a52
hiroyuki.yano <tech.hy.1625@gmail.com>
2021-07-20 01:37:58 +0000

    add favorites function

--- resources/views/user_favorite/favorite_button.blade.php
+++ resources/views/user_favorite/favorite_button.blade.php
@@ -1,14 +1,14 @@
 
-    @if (Auth::user()->is_favorite($micropost->id))
-        {{-- アンフォローボタンのフォーム --}}
-        {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-            {!! Form::submit('Unfavorite', ['class' => "btn btn-success btn-sm"]) !!}
-        {!! Form::close() !!}
-    @else
-        {{-- フォローボタンのフォーム --}}
-        {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
-            {!! Form::submit('Favorite', ['class' => "btn btn-light btn-sm"]) !!}
-        {!! Form::close() !!}
-    @endif
+@if (Auth::user()->is_favorite($micropost->id))
+    {{-- アンフォローボタンのフォーム --}}
+    {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
+        {!! Form::submit('Unfavorite', ['class' => "btn btn-success btn-sm"]) !!}
+    {!! Form::close() !!}
+@else
+    {{-- フォローボタンのフォーム --}}
+    {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
+        {!! Form::submit('Favorite', ['class' => "btn btn-light btn-sm"]) !!}
+    {!! Form::close() !!}
+@endif
 
 
